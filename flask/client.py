668f831ad202a722ac950703e9677097a3f9bd46
Tarık Korucuoğlu <91344478+taro544@users.noreply.github.com>
2023-04-16 12:45:35 +0300

    Update client.py

--- flask/client.py
+++ flask/client.py
@@ -1,9 +1,20 @@
 import requests
+import socket
 
-response = requests.get('http://localhost:80')
+ip_address = "" #Change here
+port = 80
 
-if response.status_code == 200:
-    output = response.json()['output']
-    print(output)
-else:
-    print(f"Error: {response.status_code} - {response.reason}")
+
+try:
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.settimeout(1)
+        s.connect((ip_address, port))
+        response = requests.get('http://{ip}'.format(ip=ip_address))
+
+        if response.status_code == 200:
+            output = response.json()['output']
+            print(output)
+        else:
+            print(f"Error: {response.status_code} - {response.reason}")
+except (socket.timeout, ConnectionRefusedError):
+    print(ip_address, "is not reachable on port", port)
